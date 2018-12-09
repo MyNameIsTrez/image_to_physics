b257999d4222e43c817105c02f8b4b86f57c7743
MyNameIsTrez <32989873+MyNameIsTrez@users.noreply.github.com>
2018-12-09 13:02:08 +0100

    added broken code for moving the pxls down when there's a black pxl below them

--- sketch.js
+++ sketch.js
@@ -1,53 +1,68 @@
-let img;
-let pxls = [];
+// let img;
+// let pxls = [];
 
-function preload() {
-  img = loadImage('flower.jpg');
-}
+// function preload() {
+//   img = loadImage('flower.jpg');
+// }
 
-function setup() {
-  createCanvas(img.width, img.height);
-  pixelDensity(1);
-  image(img, 0, 0);
-  loadPixels();
+// function setup() {
+//   createCanvas(img.width, img.height);
+//   pixelDensity(1);
+//   image(img, 0, 0);
+//   loadPixels();
 
-  createCanvas(img.width, img.height + 50);
-  background(150);
+//   createCanvas(img.width, img.height + 50);
 
-  for (let y = 0; y < img.height; y++) {
-    for (let x = 0; x < img.width; x++) {
-      let index = x + y * img.width;
-      let newi = index * 4;
-      let clr = [pixels[newi], pixels[newi + 1], pixels[newi + 2], pixels[newi + 3]];
-      let pxl = new Pxl(x, y, clr, index);
-      pxls.push(pxl);
-    }
-  }
-}
+//   for (let y = 0; y < img.height; y++) {
+//     for (let x = 0; x < img.width; x++) {
+//       let index = (x + y * img.width) * 4;
+//       let clr = [pixels[index], pixels[index + 1], pixels[index + 2], pixels[index + 3]];
+//       let pxl = new Pxl(x, y, clr, index);
+//       pxls.push(pxl);
+//     }
+//   }
 
-function draw() {
-  for (let pxl in pxls) {
-    pxls[pxl].move();
-    pxls[pxl].draw();
-  }
-}
+//   background(0);
+//   for (pxl = pxls.length - 2 * img.height; pxl < pxls.length; pxl++) {
+//     pxls[pxl].draw();
+//   }
+//   loadPixels();
+//   for (pxl = pxls.length - 2 * img.height; pxl < pxls.length; pxl++) {
+//     pxls[pxl].move();
+//   }
+// }
 
-class Pxl {
-  constructor(x, y, clr, index) {
-    this.x = x;
-    this.y = y;
-    this.clr = clr;
-    this.index = index;
-  }
+// function draw() {
+//   // background(0);
+//   // for (let pxl in pxls) {
+//   //   pxls[pxl].draw();
+//   // }
+//   // loadPixels();
+//   // for (let pxl in pxls) {
+//   //   pxls[pxl].move();
+//   // }
+// }
 
-  move() {
-    if (this.y < height - 1) {
-      this.y = this.y + 1;
-    }
-  }
+// class Pxl {
+//   constructor(x, y, clr, index) {
+//     this.x = x;
+//     this.y = y;
+//     this.clr = clr;
+//     this.index = index;
+//   }
 
-  draw() {
-    stroke(this.clr);
-    point(this.x, this.y);
-  }
-}
+//   move() {
+//     // console.log(this.clr, [pixels[this.index + img.width], pixels[this.index + img.width + 1], pixels[this.index + img.width + 2], pixels[this.index + img.width + 3]]);
+//     if (pixels[this.index + img.width] === 0 && pixels[this.index + img.width + 1] === 255 && pixels[this.index + img.width + 2] === 0 && pixels[this.index + img.width + 3] === 0) {
+//       this.y = this.y + 1;
+//       if (this.index === 500) {
+//         // console.log("test");
+//       }
+//     }
+//   }
+
+//   draw() {
+//     stroke(this.clr);
+//     point(this.x, this.y);
+//   }
+// }
